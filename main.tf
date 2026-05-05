aa9d7df8d9be4b84bdbe3072e574f74533c31314
villitaqt <diegovillaju@gmail.com>
2026-05-05 16:01:21 -0500

    feat: terraform plan para la infraestructura de red succeded

--- main.tf
+++ main.tf
@@ -62,3 +62,82 @@ resource "aws_subnet" "private_b" {
 
   tags = { Name = "priv-sub-b-${terraform.workspace}" }
 }
+
+
+# ==========================================
+# 5. ELASTIC IPs PARA NAT GATEWAYS
+# ==========================================
+resource "aws_eip" "nat_a" { domain = "vpc" }
+resource "aws_eip" "nat_b" { domain = "vpc" }
+
+# ==========================================
+# 6. NAT GATEWAYS (En subredes públicas)
+# ==========================================
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_a.id
+  subnet_id     = aws_subnet.public_a.id
+  tags          = { Name = "nat-a-${terraform.workspace}" }
+}
+
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_b.id
+  subnet_id     = aws_subnet.public_b.id
+  tags          = { Name = "nat-b-${terraform.workspace}" }
+}
+
+# ==========================================
+# 7. TABLAS DE RUTEO
+# ==========================================
+
+# Tabla Pública (Va al Internet Gateway)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "rt-public-${terraform.workspace}" }
+}
+
+# Tabla Privada A (Va al NAT A)
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+  tags = { Name = "rt-priv-a-${terraform.workspace}" }
+}
+
+# Tabla Privada B (Va al NAT B)
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+  tags = { Name = "rt-priv-b-${terraform.workspace}" }
+}
+
+# ==========================================
+# 8. ASOCIACIONES (Conectar tablas con subredes)
+# ==========================================
+resource "aws_route_table_association" "pub_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "pub_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "priv_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table_association" "priv_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
